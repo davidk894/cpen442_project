aefaa1a872a2a9761b09e7235b83749ab1e5b3bc
davidk894 <davidkuo94@gmail.com>
2016-11-07 14:02:58 -0800

    Utilty for POST req to server with example

---
+++ mobile_app/securefileshare/app/src/main/java/cpen442/securefileshare/HttpRequestUtility.java
@@ -0,0 +1,132 @@
+package cpen442.securefileshare;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class HttpRequestUtility extends AsyncTask<Void, Void, String> {
+
+    private String requestURL;
+    private Context mContext;
+    private String JSONString;
+    private int requestType;
+
+    public static final int GET_METHOD = 0,
+                            POST_METHOD = 1,
+                            PUT_METHOD = 2,
+                            HEAD_METHOD = 3,
+                            DELETE_METHOD = 4,
+                            TRACE_METHOD = 5,
+                            OPTIONS_METHOD = 6;
+
+
+    public static final int TIMEOUT = 60 * 1000;
+
+    public HttpRequestUtility(Context ctx) {
+        this.mContext = ctx;
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+        String response = "";
+        try {
+            URL url = new URL(this.getRequestURL());
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            this.setRequestMethod(conn);
+            conn.setConnectTimeout(HttpRequestUtility.TIMEOUT);
+            conn.setReadTimeout(HttpRequestUtility.TIMEOUT);
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            this.setHeaderData(conn);
+            this.setJSONParams(conn);
+            conn.connect();
+
+            if (conn.getResponseCode() < HttpURLConnection.HTTP_BAD_REQUEST) {
+                response = this.getResponse(conn.getInputStream());
+            } else {
+                response = this.getResponse(conn.getErrorStream());
+            }
+            conn.disconnect();
+
+        } catch (MalformedURLException e1) {
+            e1.printStackTrace();
+        } catch (IOException e2) {
+            e2.printStackTrace();
+        }
+        return response;
+    }
+
+    // #region setters and getters
+    private String getRequestURL() {
+        return this.requestURL;
+    }
+    public void setRequestURL(String s) {
+        this.requestURL = s;
+    }
+
+    private int getRequestType() {
+        return this.requestType;
+    }
+    public void setRequestType(int type) {
+        this.requestType = type;
+    }
+
+    private String getJSONString() {
+        return this.JSONString;
+    }
+    public void setJSONString(String params) {
+        this.JSONString = params;
+    }
+    // #endregion setters and getters
+
+    // #region request building
+    private void setHeaderData(HttpURLConnection conn) throws IOException {
+        conn.setRequestProperty("Content-Type", "application/json");
+        conn.setRequestProperty("Accept", "application/json");
+    }
+
+    private void setRequestMethod(HttpURLConnection conn) throws ProtocolException {
+        switch(this.getRequestType()) {
+            case HttpRequestUtility.POST_METHOD:
+                conn.setRequestMethod("POST");
+                break;
+            default:
+                // do nothing for now
+                break;
+        }
+    }
+    private void setJSONParams(HttpURLConnection conn) throws IOException {
+        if (this.getJSONString() != null) {
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+            writer.write(this.getJSONString());
+            writer.flush();
+            writer.close();
+            os.close();
+        }
+    }
+
+    private String getResponse(InputStream is) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuffer response = new StringBuffer();
+        String line;
+        while((line = reader.readLine()) != null) {
+            response.append(line);
+        }
+        reader.close();
+        return response.toString();
+    }
+    // #endregion request building
+}
--- mobile_app/securefileshare/app/src/main/java/cpen442/securefileshare/MainActivity.java
+++ mobile_app/securefileshare/app/src/main/java/cpen442/securefileshare/MainActivity.java
@@ -3,11 +3,35 @@ package cpen442.securefileshare;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.createAccountRequestExample();
+    }
+
+    private void createAccountRequestExample() {
+        HttpRequestUtility request = new HttpRequestUtility(getApplicationContext());
+        request.setRequestType(HttpRequestUtility.POST_METHOD);
+        request.setRequestURL("https://zb9evmcr90.execute-api.us-west-2.amazonaws.com/Prod/create-account");
+
+        JSONObject requestParams = new JSONObject();
+        try {
+            requestParams.put("name", "AuckFndrew");
+            requestParams.put("IMEI", "AuckFndrew");
+            requestParams.put("contactNumber", "0987654321");
+            requestParams.put("firebaseID", "AuckFndrew");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        request.setJSONString(requestParams.toString());
+        request.execute();
     }
 }
